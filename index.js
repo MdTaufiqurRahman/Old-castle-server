f401baa51db2b1657f45173679c6306ff30b1716
taufiqur.anik.bd <taufiqur.anik.bd@gmail.com>
2022-09-12 19:26:00 +0600

    auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 
 dotenv.config();
@@ -13,7 +14,8 @@ mongoose
     console.error(err);
   });
 
-app.use(express.json);
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
