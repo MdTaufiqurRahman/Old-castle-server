de72ae708226c793084985a690c0a411a4d2f046
taufiqur.anik.bd <taufiqur.anik.bd@gmail.com>
2022-09-13 11:21:35 +0600

    update

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,33 @@
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send('user test is successful');
-});
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
 
-router.post("/usertestpost", (req, res) => {
-    const username = req.body.username;
-    res.send("your user name is: " + username);
-})
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
