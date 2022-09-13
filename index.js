8f33292f47fe63c2e7eb445eb998adf4e90ea9b0
taufiqur.anik.bd <taufiqur.anik.bd@gmail.com>
2022-09-13 12:04:34 +0600

    CRUD Operation - cart and product

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,9 +18,13 @@ mongoose
     console.error(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend Server is running`);
