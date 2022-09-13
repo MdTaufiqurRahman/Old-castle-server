f28ea04947bb71513b4a07de18c394b236696986
taufiqur.anik.bd <taufiqur.anik.bd@gmail.com>
2022-09-13 13:45:17 +0600

    CRUD Operation - cart and product

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+
 const cors = require("cors");
 
 dotenv.config();
@@ -25,6 +27,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend Server is running`);
